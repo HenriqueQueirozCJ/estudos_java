1cad175ca6947516204f92a60e6c25875d454571
Henrique Queiroz <henriquequeiroz.ads@hotmail.com>
2024-01-15 11:17:01 -0400

    novos

---
+++ estudojava.java
@@ -0,0 +1,9 @@
+public class estudojava {
+    public static void main(String[] args) {
+        int ano = 2024;
+        int a=1, b=3;
+
+        System.out.println("Fametro " + ano);
+        System.out.println((float) a / b );
+    }
+}
